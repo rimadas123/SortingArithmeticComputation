418a6212bf58c90bcf904f89e09529f44fa77aef
rimadas123 <rdas5969@gmail.com>
2020-02-27 16:34:39 +0530

    Added : Sorted the Result and Computated in Ascending Order

--- sortingArithmeticProblem.sh
+++ sortingArithmeticProblem.sh
@@ -42,7 +42,7 @@ do
 done
 echo "array:${array[@]}"
 
-#Sort results in descending order
+#Sorted result in descending order
  length1="${#array[@]}"
    for (( i=0; i<$length1; i++ ))
    do
@@ -57,3 +57,19 @@ echo "array:${array[@]}"
       done
    done
 echo "Sorted in descending:"${array[@]}
+
+#sorted result in ascending order
+length1="${#array[@]}"
+   for (( i=0; i<$length1; i++ ))
+   do
+      for (( j=0; j<$length1-1; j++ ))
+      do
+         if (( $(echo "${array[j]} > ${array[j+1]}" |bc -l) ))
+         then
+            temp=${array[j]}
+            array[j]=${array[j+1]}
+            array[j+1]=$temp
+         fi
+      done
+   done
+echo "Sorted the values - ascending order: ${array[@]}"
